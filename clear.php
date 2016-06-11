a62f7259702f3d45107b7ec4d3d3b8bd2480a177
root <root@localhost>
2016-06-11 15:41:16 -0400

    Increasing room name length max from 10 to 200

--- clear.php
+++ clear.php
@@ -1,6 +1,6 @@
 <?php
 
-$clean_room=strip_tags( (strlen($_GET['room']) > 10) ? substr($_GET['room'],0,10) : $_GET['room'] );
+$clean_room=strip_tags( (strlen($_GET['room']) > 50) ? substr($_GET['room'],0,50) : $_GET['room'] );
 echo "woot";
 
 header('Location: /?room='.$_GET["room"]);
